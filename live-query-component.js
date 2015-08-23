9ad7122a00ed7b8d13cbd1a2295696e9af2ed39c
Jiri Spac <capajj@gmail.com>
2015-08-23 12:51:36 +0200

    updated to work without setting a state upfront

--- live-query-component.js
+++ live-query-component.js
@@ -1,29 +1,40 @@
 module.exports = function LQComponent(component) {
-	if (!component.state) {
-		component.state = {};
-	}
-	var iterateQueries = function(fn) {
-		var keys = Object.keys(component.queries);
-		var key;
-		while (key = keys.pop()) {
-			fn(key, component.queries[key]);
-		}
-	};
+  if (!component.state) {
+    component.state = {};
+  }
+  var iterateQueries = function(fn) {
+    var keys = Object.keys(component.queries);
+    var key;
+    while (key = keys.pop()) {
+      fn(key, component.queries[key]);
+    }
+  };
 
-	iterateQueries(function(key, query) {
-    query.exec();
-		query.on('any', function() {
+  if (component.state === undefined) {
+    component.state = {};
+  }
+  iterateQueries(function(key, query) {
+
+    query = query.exec();
+    if (query.indexedByMethods.findOne) {
+      component.state[key] = null;
+    } else if (query.indexedByMethods.count) {
+      component.state[key] = 0;
+    } else {
+      component.state[key] = [];
+    }
+    query.on('any', function() {
       var docsOnKeyProp = {};
       docsOnKeyProp[key] = query.result;
-			component.setState(docsOnKeyProp);
-		});
-	});
+      component.setState(docsOnKeyProp);
+    });
+  });
 
-	var originalUnmount = component.componentWillUnmount;
-	component.componentWillUnmount = function() {
-		iterateQueries(function(key, LQ) {
-			LQ.stop();
-		});
-		originalUnmount.apply(component, arguments);
-	}
+  var originalUnmount = component.componentWillUnmount;
+  component.componentWillUnmount = function() {
+    iterateQueries(function(key, LQ) {
+      LQ.stop();
+    });
+    originalUnmount.apply(component, arguments);
+  }
 };
